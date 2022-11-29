0aff2df13732123213af9f66d410aaf6913c1d83
fariawillyan <fariawillyan@gmail.com>
2022-11-29 08:46:15 -0300

    Data-Types_Primitives

---
+++ src/Chapter_1_Building_Blocks/Data_Types_Primitives.java
@@ -0,0 +1,34 @@
+package Chapter_1_Building_Blocks;
+
+
+/**
+ * @author Willyan faria
+ *
+ * types primitive is just a single value in memory. is not an objet in java.
+ */
+
+
+public class Data_Types_Primitives {
+
+    // types primitive
+
+
+    boolean x; //true or false
+
+    //decimal
+    byte a; //8 bit    123
+    short b; //16 bit  123
+    int c;// 32 bit   123
+    long d; // 64 bit  123L
+
+    //
+    float f;// 32 bit  123.45f
+    double g;// 64 bit 123.456
+    char h;// 16 bit
+
+    String i;// is a object and not type primitive.
+    // long max = 321654897; // does not compile
+    // long max = 321654987L; // now java it is a long
+    // int million = 1000000;
+    //int million = 1_000_000; // You can use underscore to make them easier to read;
+}
