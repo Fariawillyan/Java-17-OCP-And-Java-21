ddac762bc9cdbee17dd7abf746ce77ccb7ca4651
fariawillyan <fariawillyan@gmail.com>
2022-12-05 20:04:41 -0300

    textblocks

---
+++ src/Chapter_1_Building_Blocks/Defining_Text_Blocks.java
@@ -0,0 +1,21 @@
+package Chapter_1_Building_Blocks;
+
+public class Defining_Text_Blocks {
+
+// String block = """ doe """; DOES NOT COMPILE
+
+/**
+ * The text block requite a line break after tje opening """;
+  */
+
+static String block = """
+        doe
+        deer""";
+
+//use "\" tells java not to add a new line before deer.
+
+    public static void main(String[] args) {
+        System.out.println(block);
+    }
+
+}
