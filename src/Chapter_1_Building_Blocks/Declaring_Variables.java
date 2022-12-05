e73e45976ff907d0c20e18de590a5b27b892b52f
fariawillyan <fariawillyan@gmail.com>
2022-12-05 20:04:22 -0300

    variables

---
+++ src/Chapter_1_Building_Blocks/Declaring_Variables.java
@@ -0,0 +1,27 @@
+package Chapter_1_Building_Blocks;
+
+/**
+ * A variable is name for a piece of memory that stores data. *
+ */
+
+public class Declaring_Variables {
+
+    String zooName = "The Best Zoo";
+
+                                    //Reserved words
+    /******************************************************************************************
+     * abstract            assert             boolean            break              byte      *
+     * case                catch              char               class              const*    *
+     * continue            default            do                 double             else      *
+     * for                 goto*              if                 implements         float     *
+     * enum                extends            final              finally            while     *
+     * instanceof          int                interface          long               import    *
+     * new                 package            private            protected          native    *
+     * return              short              static             strictfp           public    *
+     * switch              synchronized        this               throw              super     *
+     * transient           try                void               volatile           throws    *
+     *****************************************************************************************/
+
+    // Java use uppercase snake case to CONSTANTS and ENUM values. EXAMPLE_JAVA_HERE
+
+}
