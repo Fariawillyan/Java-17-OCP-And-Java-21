9f3853e88aec87b2225788bfd9c690066b739240
fariawillyan <fariawillyan@gmail.com>
2023-01-02 08:34:33 -0300

    Operators

---
+++ src/Chapter_2_Operators/Operator_Precedence.java
@@ -0,0 +1,17 @@
+package Chapter_2_Operators;
+
+public class Operator_Precedence {
+
+    //java supports threee flavors of operators: unary, binary and ternary.
+
+    public static void main(String[] args) {
+        int cookies = 4;
+        double reward = 3 + 2 * --cookies;
+        System.out.println( "Zoo animal receives: " +reward+ " reward points");
+
+        // the multiplication operator (*) has a higher precedente.
+        // the assignment operator (=) has the lowest order of precedente.
+
+    }
+
+}
