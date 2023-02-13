eaf42d7b8682c36491dc0d54d94c16b253c643a5
WILLYAN FARIA <fariawillyan@gmail.com>
2023-02-13 08:44:33 -0300

    Examples methods string

---
+++ src/Chapter_4_Core_APISs/Strings.java
@@ -0,0 +1,43 @@
+package Chapter_4_Core_APISs;
+
+/**
+ *
+ * Any example methods in Strings
+ *
+ */
+
+public class Strings {
+
+
+    public static void main(String[] args) {
+
+        String example = "Hello world";
+
+
+        System.out.println(example.length()); //return numbers of characteres in the string.
+        System.out.println(example.charAt(5));// return characteres is a specific position.
+        System.out.println(example.indent('e')); //add same numbers of blank spaces
+        System.out.println(example.substring(3)); // find and return parts of the string.
+        System.out.println(example.substring(example.indexOf('w'))); //find and return index/
+        System.out.println(example.toUpperCase());// or lowercase // convert strings.
+
+        //boolen
+        System.out.println(example.equals("Hello world")); // checks objets contain same order
+        System.out.println(example.equalsIgnoreCase("hello world")); // ignore uppercase or lowercase
+        System.out.println(example.startsWith("a")); //is contained and start letter ?
+        System.out.println(example.isBlank()); // is blank ?
+        System.out.println(example.isEmpty()); // is empty?
+
+        //others
+        System.out.println(example.replace('H', 'R')); //replace values
+        System.out.println(example.trim()); // remove space
+        System.out.println(example.strip()); // remove space and support unicode
+
+        //example
+        String exampleTwo = "Hello world atomic       ".trim().toLowerCase().replace('H', 'a');
+        System.out.println(exampleTwo);
+
+    }
+
+
+}
