187e335b745513fb766dfa8214cd7918957440e5
fariawillyan <fariawillyan@gmail.com>
2023-03-30 08:50:26 -0300

    methods

---
+++ src/Chapter_5_Methods/Methods.java
@@ -0,0 +1,23 @@
+package Chapter_5_Methods;
+
+
+
+public class Methods {
+
+//tow of the parts - the method name and parameter list - are called the method signature.
+//acess modifiers -> private, public, protected and package access.
+
+
+        /** optional specifiers
+
+        static -> shared class object
+        abstract -> used in an abstract class
+        final -> not be overridden in a subclass
+        default -> used i na interface to provide a default implementation
+        syncronized -> used with multithread code
+        native -> used when interacting with code written in another language, such c++
+        strictfp -> used for making floating-point calculations portable
+
+         **/
+
+}
---
+++ src/Chapter_5_Methods/Methods_examples.java
@@ -0,0 +1,59 @@
+package Chapter_5_Methods;
+
+
+
+public class Methods_examples {
+
+    int hunger = 4;
+
+
+
+/* return type other than void are required to have a return. This return statement
+ must include the primitive or object to be returned.*/
+
+
+    int getHeight1(){
+        int temp = 9;
+        return temp;
+    }
+
+    void nap(){
+    }
+
+    //parameter List and method signature
+    public void visitZoo(String name, int waitTime){
+
+    }
+    public void oneExeption() throws IllegalArgumentException{}
+
+
+
+    //declaring local and instance variable
+    public int feedZooAnimals(){
+        int snack = 10;
+
+        if(snack > 4){
+            long dinnerTime = snack++;
+            hunger--;
+        }
+        return snack;
+    }
+
+    public void zooAnimalCheckup(boolean isWeekend){
+        final int rest;
+
+        if(isWeekend) rest = 5; else rest = 20;
+        System.out.println(rest);
+
+    }
+
+    public static void main(String[] args) {
+    Methods_examples teste = new Methods_examples();
+     teste.zooAnimalCheckup(false);
+        System.out.println(teste);
+    }
+
+
+
+}
+
